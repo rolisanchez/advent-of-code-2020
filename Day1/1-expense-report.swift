8c6de3f4a6e419a51bf124a161df113c7d3379df
rolisanchez <roli.s91@gmail.com>
2020-12-01 18:17:56 -0300

    Day 1 part 1

---
+++ Day1/1-expense-report.swift
@@ -0,0 +1,47 @@
+import Foundation
+
+// Use Hash Table Data Structure to find sum
+
+let filepath = "./input.txt"
+
+
+do {
+    if let contents = try? String(contentsOfFile: filepath) {
+        let lines = contents.components(separatedBy: "\n")
+
+        var hashTable =  [Int:Int?]()
+        
+        for line in lines {
+            guard !line.isEmpty else { continue }
+
+            let lineInt = Int(line.trimmingCharacters(in: .whitespacesAndNewlines))!
+
+            hashTable[lineInt] = 0
+
+        }
+
+        print("Finished inserting in hash table")
+
+        let target = 2020
+            
+            var val1: Int!
+            var val2: Int!
+
+            for val in hashTable.keys {
+                let y = target - val
+                if hashTable.keys.contains(y) {
+                    val1 = val
+                    val2 = y
+                    break
+                }
+            }
+
+            print("val1: ", val1)
+            print("val2: ", val2)
+            
+            print("product: ", (val1*val2))
+    }
+    else {
+        fatalError("Could not open file")
+    }
+}
