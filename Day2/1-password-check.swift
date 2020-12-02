0fa3d5be9b2b738ab1975af33896b693a2a3fe71
rolisanchez <roli.s91@gmail.com>
2020-12-02 14:29:19 -0300

    Day2 part 1

---
+++ Day2/1-password-check.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+// Use Hash Table Data Structure to find sum
+
+let filepath = "./input.txt"
+
+
+do {
+    if let contents = try? String(contentsOfFile: filepath) {
+        let lines = contents.components(separatedBy: "\n")
+
+        var validCounts = 0
+        for line in lines {
+            guard !line.isEmpty else { continue }
+
+            let lineComponents = line.components(separatedBy: ":")
+            let policyComponents = lineComponents[0].components(separatedBy: " ")
+            let policyCountComponents = policyComponents[0].components(separatedBy: "-") // ex: 8-11
+            let minCount = Int(policyCountComponents[0])!
+            let maxCount = Int(policyCountComponents[1])!
+            let policyLetter = policyComponents[1]
+            let password = lineComponents[1].trimmingCharacters(in: .whitespacesAndNewlines)
+
+            
+            let range = NSRange(location: 0, length: password.utf16.count)
+            let regex = try! NSRegularExpression(pattern: "\(policyLetter){1,1}")
+
+            let results = regex.matches(in: password, range: range)
+
+            if results.count >= minCount && results.count <= maxCount {
+                validCounts += 1
+            }
+            // print("results: ", results.count)
+
+        }
+
+        print("Finished reading lines")
+        print("validCounts: ", validCounts)
+    }
+    else {
+        fatalError("Could not open file")
+    }
+}
