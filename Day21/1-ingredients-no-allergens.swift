d3a4e16d944a99c8ce5047dbe431d9a861675f86
rolisanchez <roli.s91@gmail.com>
2020-12-22 11:33:21 -0300

    Day 21 Part 2

--- Day21/1-ingredients-no-allergens.swift
+++ Day21/1-ingredients-no-allergens.swift
@@ -63,7 +63,74 @@ func getIngredientsAndAllergens(_ lines: [String]){
             }
         }
     }
-    print("Result: ", appearingCount)
+    print("Result Part 1: ", appearingCount)
+
+    var allergicIngredientsToAllergens = [String:[Set<String>]]()
+    for (key, value) in ingredientsToAllergens {
+        let newValue = key.subtracting(safeIngredients)
+        
+        for newKey in value {
+            if allergicIngredientsToAllergens[newKey] == nil {
+                allergicIngredientsToAllergens[newKey] = [newValue]
+            } else {
+                allergicIngredientsToAllergens[newKey]!.append(newValue)
+            }
+        }
+
+        
+    }
+    var reducedAllergicToAllerg = [String:Set<String>]()
+    
+    for (key, sets) in allergicIngredientsToAllergens {
+        var returnSet = Set<String>()
+        
+        if sets.count > 0 {
+            returnSet = returnSet.union(sets[0])
+            let rest = sets[1..<sets.count]
+            returnSet = rest.reduce(returnSet){ $0.intersection($1)}
+        }
+        reducedAllergicToAllerg[key] = returnSet
+    }
+
+    var sortedIngAllerg = reducedAllergicToAllerg.sorted{$0.value.count < $1.value.count}
+    var allergenMatches = [String:String]()
+    let count = sortedIngAllerg.count
+    while allergenMatches.count < count {
+        var valueToRemove: String? = nil
+        for (key, value) in sortedIngAllerg {
+            if value.count == 1 {
+                allergenMatches[key] = value[value.startIndex]
+                reducedAllergicToAllerg[key] = nil
+                
+                valueToRemove = value[value.startIndex]
+            }
+            if let remove = valueToRemove {
+                for (key2, value2) in reducedAllergicToAllerg {
+                    if value2.contains(remove){
+                        var newValue2 = value2
+                        
+                        newValue2 = newValue2.filter{$0 != remove}
+                        reducedAllergicToAllerg[key2] = newValue2
+                    }
+
+                }
+                sortedIngAllerg = reducedAllergicToAllerg.sorted{$0.value.count < $1.value.count} 
+                break
+            }
+            
+        }
+    }
+
+
+    print("allergenMatches sorted: ", allergenMatches.sorted{$0.key < $1.key})
+
+    var part2ans = [String]()
+    allergenMatches.sorted{$0.key < $1.key}.forEach { _, val in
+        part2ans.append(val)
+    }
+    print("Result Part 2: ", part2ans.joined(separator: ","))
+    
+
 }
 
 extension StringProtocol {
